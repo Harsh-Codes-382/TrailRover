8c1c1d5f58cbdfde9da5d99a888b47ac2986637e
Harsh-Codes-382 <harshkumar3826@gmail.com>
2024-01-25 19:52:22 +0530

    It is just fecthing the data using Infinite Scroll and sorting the data on bases on couple of features

---
+++ src/pages/explore/Explore.jsx
@@ -0,0 +1,172 @@
+import React, { useState, useEffect } from "react";
+import { generatePath, useParams } from "react-router-dom";
+import InfiniteScroll from "react-infinite-scroll-component";
+import Select from "react-select";  // This is React library to select the items
+
+import "./style.scss";
+
+import useFetch from "../../hooks/useFetch";
+import { fetchDataFromApi } from "../../utils/api";
+import ContentWrapper from "../../components/contentWrapper/contentWrapper";
+import MovieCard from "../../components/movieCard/MovieCard";
+import Spinner from "../../components/spinner/Spinner";
+
+let filters = {};
+
+const sortbyData = [
+    { value: "popularity.desc", label: "Popularity Descending" },   // label = means which is appear to user to select
+    { value: "popularity.asc", label: "Popularity Ascending" },     // vlaue = means which will be passed to the filters object and filter obj will be sent to url fro api call
+    { value: "vote_average.desc", label: "Rating Descending" },
+    { value: "vote_average.asc", label: "Rating Ascending" },
+    {
+        value: "primary_release_date.desc",
+        label: "Release Date Descending",
+    },
+    { value: "primary_release_date.asc", label: "Release Date Ascending" },
+    { value: "original_title.asc", label: "Title (A-Z)" },
+];
+
+const Explore = () => {
+    const [data, setData] = useState(null);
+    const [pageNum, setPageNum] = useState(1);
+    const [loading, setLoading] = useState(false);
+    const [genre, setGenre] = useState(null); // to hold the selected genre
+    const [sortby, setSortby] = useState(null); // to hold the type of sort which user select
+    const { mediaType } = useParams();
+
+    const { data: genresData } = useFetch(`/genre/${mediaType}/list`);
+    console.log(genresData);
+
+    const fetchInitialData = () => {
+        setLoading(true);
+        // we are passig filters here and filtered result will come 
+        fetchDataFromApi(`/discover/${mediaType}`, filters).then((res) => {
+            setData(res);
+            setPageNum((prev) => prev + 1);
+            setLoading(false);
+        });
+    };
+
+    const fetchNextPageData = () => {
+        fetchDataFromApi(
+            `/discover/${mediaType}?page=${pageNum}`,
+            filters
+        ).then((res) => {
+            if (data?.results) {
+                setData({
+                    ...data,
+                    results: [...data?.results, ...res.results],
+                });
+            } else {
+                setData(res);
+            }
+            setPageNum((prev) => prev + 1);
+        });
+    };
+
+    useEffect(() => { // we want to do these changes everytime somebody switches from movies to Tv show or vice-versa
+        filters = {};
+        setData(null);
+        setPageNum(1);
+        setSortby(null);
+        setGenre(null);
+        fetchInitialData();
+    }, [mediaType]);
+
+    const onChange = (selectedItems, action) => {
+        if (action.name === "sortby") { // if somebody selected the sortBy then set That sort type in state
+            setSortby(selectedItems);
+            if (action.action !== "clear") {    // in select category if user hadn't pressed the close icon then we are setting the sort type in filter from the value of selected sort type. Value of sort type is in array above
+                filters.sort_by = selectedItems.value;
+            } else {
+                delete filters.sort_by; //  But if user pressed the close icon then delete the filter
+            }
+        }
+
+        if (action.name === "genres") { // if user selects the genre category 
+            setGenre(selectedItems);
+            if (action.action !== "clear") {
+                let genreId = selectedItems.map((g) => g.id); // from genre array from api call of genre we are only storing the id of genre in variable genreId. So, it holds the id of genre
+
+                genreId = JSON.stringify(genreId).slice(1, -1); // because we need to pass only the id of genre & multiple genre so we made a array of genre id above e.g= "["12", "23"]" but we can't pass the array brackets so we slice it & just because we need to slice the brackets we converted the genreId array into sting and only "12", "23" is passed ino filter obj in "with_genre"
+
+                filters.with_genres = genreId;  // with_genre is a api endpoint we need the id's of genre inside it
+            } else {
+                delete filters.with_genres;
+            }
+        }
+
+        setPageNum(1);  // Everytime onChange it is set the pageNum = 1 and call the api & show the results on basisalong with filter obj 
+        fetchInitialData();
+    };
+
+    return (
+        <div className="explorePage">
+            <ContentWrapper>
+                <div className="pageHeader">
+                    <div className="pageTitle">
+                        {mediaType === "tv"
+                            ? "Explore TV Shows"
+                            : "Explore Movies"}
+                    </div>
+                    <div className="filters">
+                        <Select
+                            isMulti
+                            name="genres"
+                            value={genre}
+                            closeMenuOnSelect={true}
+                            options={genresData?.genres}
+                            getOptionLabel={(option) => option.name}
+                            getOptionValue={(option) => option.id}
+                            onChange={onChange} // here we passed the method which executes when data changes in select category
+                            placeholder="Select genres"
+                            className="react-select-container genresDD"
+                            classNamePrefix="react-select"
+                        />
+                        <Select
+                            name="sortby"
+                            value={sortby}
+                            options={sortbyData}
+                            onChange={onChange}
+                            isClearable={true}
+                            placeholder="Sort by"
+                            className="react-select-container sortbyDD"
+                            classNamePrefix="react-select"
+                        />
+                    </div>
+                </div>
+                {loading && <Spinner initial={true} />}
+                {!loading && (
+                    <>
+                        {data?.results?.length > 0 ? (
+                            <InfiniteScroll
+                                className="content"
+                                dataLength={data?.results?.length || []}
+                                next={fetchNextPageData}
+                                hasMore={pageNum <= data?.total_pages}
+                                loader={<Spinner />}
+                            >
+                                {data?.results?.map((item, index) => {
+                                    if (item.media_type === "person") return;
+                                    return (
+                                        <MovieCard
+                                            key={index}
+                                            data={item}
+                                            mediaType={mediaType}
+                                        />
+                                    );
+                                })}
+                            </InfiniteScroll>
+                        ) : (
+                            <span className="resultNotFound">
+                                Sorry, Results not found!
+                            </span>
+                        )}
+                    </>
+                )}
+            </ContentWrapper>
+        </div>
+    );
+};
+
+export default Explore;
